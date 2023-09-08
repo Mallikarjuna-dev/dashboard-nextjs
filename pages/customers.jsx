93890770a8737f3ec1b36c175a0b0c826787e537
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2023-09-08 04:54:32 +0000

    user router page

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -21,12 +21,23 @@ const customers = () => {
             {data.map((order, id) => (
               <li
                 key={id}
-                className="bg-gray-50 hover:bg-gray-200 my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 rounded-lg cursor-pointer"
+                className="bg-gray-50 hover:bg-gray-200 my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between rounded-lg cursor-pointer"
               >
                 <div className="flex items-center">
                   <div className="bg-purple-100 p-3 rounded-lg">
                     <BsPersonFill className="text-purple-800" />
                   </div>
+                  <p className="pl-4">
+                    {order.name.first + " " + order.name.last}
+                  </p>
+                </div>
+                <p className="sm:text-left text-right text-gray-600">
+                  {order.name.first}@gmail.com
+                </p>
+                <p className="hidden md:flex">{order.date}</p>
+                <div className="hidden sm:flex justify-between items-center">
+                  <p>{order.method}</p>
+                  <BsThreeDotsVertical />
                 </div>
               </li>
             ))}
