634f63c81dfd907181fde652a7d1d93944110bc3
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2023-09-11 11:07:16 +0000

    user settigs

---
+++ pages/settings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const settings = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h2 className="text-lg font-semibold">Under development...</h2>
+    </div>
+  );
+};
+
+export default settings;
